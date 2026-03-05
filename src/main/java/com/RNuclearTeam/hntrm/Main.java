60f6e836e10e78ed6f0c35228b601d4b2c56741f
R3YPER <r3yper3@gmail.com>
2026-03-05 08:31:42 +0200

    Starting a project, add aluminium_ore block

---
+++ src/main/java/com/RNuclearTeam/hntrm/Main.java
@@ -0,0 +1,39 @@
+
+package com.RNuclearTeam.hntrm;
+
+import com.RNuclearTeam.hntrm.register.RegBlocks;
+import com.RNuclearTeam.hntrm.register.RegItems;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Main.MODID)
+public class Main {
+
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "hntrm";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Main() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        RegBlocks.register(modEventBus);
+        RegItems.register(modEventBus);
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+}
