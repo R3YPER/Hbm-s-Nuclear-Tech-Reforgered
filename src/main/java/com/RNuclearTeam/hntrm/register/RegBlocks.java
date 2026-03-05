fbbabb91d1842143ada8085015d1552cb416616d
R3YPER <r3yper3@gmail.com>
2026-03-05 08:42:46 +0200

    Starting a project, add aluminium_ore block

---
+++ src/main/java/com/RNuclearTeam/hntrm/register/RegBlocks.java
@@ -0,0 +1,33 @@
+package com.RNuclearTeam.hntrm.register;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+import static com.RNuclearTeam.hntrm.Main.MODID;
+
+public class RegBlocks {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    public static final RegistryObject<Block> ALUMINIUM_ORE = registerBlock("aluminium_ore", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)));
+
+    private static <T extends Block> RegistryObject<T> registerBlock (String name, Supplier<T> block) {
+        RegistryObject<T> ret = BLOCKS.register(name, block);
+        registerBlockItem(name, ret);
+        return ret;
+    }
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
+        return RegItems.ITEMS.register(name, ()-> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
---
+++ src/main/java/com/RNuclearTeam/hntrm/register/RegItems.java
@@ -0,0 +1,16 @@
+package com.RNuclearTeam.hntrm.register;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import static com.RNuclearTeam.hntrm.Main.MODID;
+
+public class RegItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
