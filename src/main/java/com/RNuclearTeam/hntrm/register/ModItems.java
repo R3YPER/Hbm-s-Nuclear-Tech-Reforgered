99446af8c15f0bf6d54e81b6d016bcceffdbce0e
R3YPER <r3yper3@gmail.com>
2026-03-08 11:07:36 +0200

    added raw aluminium item

--- src/main/java/com/RNuclearTeam/hntrm/register/ModItems.java
+++ src/main/java/com/RNuclearTeam/hntrm/register/ModItems.java
@@ -4,11 +4,14 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 import static com.RNuclearTeam.hntrm.Main.MODID;
+import static net.minecraft.world.item.Items.registerItem;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    public static final RegistryObject<Item> RAW_ALUMINIUM = ITEMS.register("raw_aluminium", ()-> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
